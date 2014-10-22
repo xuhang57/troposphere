041aebaf41f6d62a4585f62178aeb403d82859ee
J. Matt Peterson <jmatt@jmatt.org>
2014-10-22 14:36:10 -0700

    Utility scripts.
    	new file:   scripts/rm_all_pyc.sh
    	new file:   scripts/set_permissions.sh

---
+++ scripts/set_permissions.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+if [[ $EUID -ne 0 ]]; then
+    echo "You must be a root user to set these permissions"
+    exit 1
+fi
+
+export ATMOSPHERE_HOME=/opt/dev/atmosphere
+
+chmod -R g+w ${ATMOSPHERE_HOME}
+
+chmod -R 644 ${ATMOSPHERE_HOME}/extras/ssh
+
+chmod 755 ${ATMOSPHERE_HOME}/extras/ssh
+
+chmod -R 600 ${ATMOSPHERE_HOME}/extras/ssh/id_rsa
+
+chown -R www-data:core-services ${ATMOSPHERE_HOME}
+
+chown -R www-data:core-services ${ATMOSPHERE_HOME}/extras/apache
+
+chown -R root:root ${ATMOSPHERE_HOME}/extras/ssh
+
