1da40025f5e0db736ad256fb120577925b68eb2f
prosif <prosif@gmail.com>
2015-07-08 14:22:41 -0700

    add badge info to globals

--- troposphere/static/js/globals.js
+++ troposphere/static/js/globals.js
@@ -3,7 +3,10 @@ define(function(require){
   return {
     API_ROOT: window.API_ROOT || '/api/v1',
     API_V2_ROOT: window.API_V2_ROOT || '/api/v2',
-    API_V2_MOCK_ROOT: window.API_V2_MOCK_ROOT
+    API_V2_MOCK_ROOT: window.API_V2_MOCK_ROOT,
+    BADGE_HOST: window.BADGE_HOST,
+    BADGE_SYSTEM: 'demo',
+    BADGE_SECRET: 'mysecret'
   }
 
 });
