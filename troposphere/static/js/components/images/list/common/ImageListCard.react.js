486b9291035f0cacdf15d098a321a86a93c2ad19
Tharon Carlson <design@tharoncarlson.com>
2016-03-24 09:46:34 -0700

    implement MediaCard on ImageListCard

--- troposphere/static/js/components/images/list/common/ImageListCard.react.js
+++ troposphere/static/js/components/images/list/common/ImageListCard.react.js
@@ -11,13 +11,28 @@ define(function (require) {
     globals = require('globals'),
     moment = require('moment'),
     momentTZ = require('moment-timezone'),
-    Router = require('react-router');
+    Router = require('react-router'),
+    MediaCard = require('components/common/ui/MediaCard.react');
 
   return React.createClass({
     displayName: "ImageListCard",
 
+    getInitialState: function() {
+        return {
+            isExpanded: false
+        }
+    },
+
     propTypes: {
       image: React.PropTypes.instanceOf(Backbone.Model).isRequired
+    },
+
+    onExpand: function() {
+        let isExpanded = this.state.isExpanded ?
+            false : true;
+        this.setState({
+            isExpanded
+        });
     },
 
     render: function () {
@@ -27,18 +42,56 @@ define(function (require) {
         imageCreationDate = moment(image.get('start_date'))
                                 .tz(globals.TZ_REGION)
                                 .format("MMM Do YYYY hh:mm a z"),
-        converter = new Showdown.Converter(),
-        description = image.get('description');
-      if(!description) {
-          description = "No Description Provided."
+        converter = new Showdown.Converter();
+      let descriptionFull = image.get('description');
+      let description;
+      let descriptionActive;
+
+      if (descriptionFull) {
+            description = descriptionFull.length < 150 ? 
+                descriptionFull :
+                (descriptionFull.substring(0,150) + " ...");
+            descriptionActive = description;
       }
-      var descriptionHtml = converter.makeHtml(description),
-        iconSize = 67,
-        icon;
+      if (!descriptionFull) {
+            descriptionActive = "No Description Provided."
+      }
+
+      if (this.state.isExpanded) {
+          descriptionActive = descriptionFull;
+
+      }
+
+      let iconSize = 40;
 
       // always use the Gravatar icons
-      icon = (
-        <Gravatar hash={image.get('uuid_hash')} size={iconSize} type={type}/>
+      let icon = (
+            <Router.Link to="image-details" params={{imageId: image.id}}>
+                    <Gravatar hash={image.get('uuid_hash')} size={iconSize} type={type}/>
+            </Router.Link>
+      );
+
+      let title = (
+            <Router.Link to="image-details" params={{imageId: image.id}}>
+                {image.get('name')}
+            </Router.Link>
+      );
+
+      let date = (
+            <div className="Media-date">
+                <time>{imageCreationDate}</time> by <strong>{image.get('created_by').username}</strong>
+            </div>
+      );
+
+      let detail = (
+            <div>
+                <div className="Media-description col-md-6">
+                    {descriptionActive}
+                </div>
+                <div className="col-md-6">
+                    <Tags activeTags={imageTags}/>
+                </div>
+            </div>
       );
 
       // Hide bookmarking on the public page
@@ -48,41 +101,18 @@ define(function (require) {
           <Bookmark image={image}/>
         );
       }
-
+      let featured = (<i className="glyphicon glyphicon-thumbs-up"/>);
       return (
-        <div className='Media Card'>
-
-            <div className='Media-img'>
-                <Router.Link to="image-details" params={{imageId: image.id}}>
-                    {icon}
-                </Router.Link>
-            </div>
-
-            <div className="Media-content">
-                <div className="row">
-                    <div className='app-name col-md-4'>
-                        <h4>
-                            <Router.Link to="image-details" params={{imageId: image.id}}>
-                            {image.get('name')}
-                            </Router.Link>
-                        </h4>
-                        <div className="Media-date">
-                            <time>{imageCreationDate}</time> by <strong>{image.get('created_by').username}</strong>
-                        </div>
-                    </div>
-                    <div className="Media-description col-md-5">
-                        {description}
-                    </div>
-                    <div className="col-md-3">
-                        <Tags activeTags={imageTags}/>
-                    </div>
-                {bookmark}
-                </div>
-            </div>
-        </div>
+            <MediaCard 
+                isExpanded={this.state.isExpanded}
+                image={icon}
+                title={title}
+                subTitle={date}
+                detail={detail}
+                header={[bookmark,featured]}
+                onExpand={this.onExpand}
+            />
       );
     }
-
   });
-
 });
