8e45d64e826ce89b420f29d67712d9b3016933e0
Tharon Carlson <design@tharoncarlson.com>
2016-03-29 11:34:36 -0700

    Fomating Image List Card

--- troposphere/static/js/components/images/list/common/ImageListCard.react.js
+++ troposphere/static/js/components/images/list/common/ImageListCard.react.js
@@ -1,99 +1,92 @@
-define(function (require) {
+import React from 'react/addons';
+import Gravatar from 'components/common/Gravatar.react';
+import Backbone from 'backbone';
+import Bookmark from 'components/images/common/Bookmark.react';
+import context from 'context';
+import Tags from 'components/images/detail/tags/Tags.react';
+import actions from 'actions';
+import stores from 'stores';
+import modals from 'modals';
+import Showdown from 'showdown';
+import globals from 'globals';
+import moment from 'moment';
+import momentTZ from 'moment-timezone';
+import Router from 'react-router';
+import MediaCard from 'components/common/ui/MediaCard.react';
 
-  var React = require('react/addons'),
-    Gravatar = require('components/common/Gravatar.react'),
-    Backbone = require('backbone'),
-    Bookmark = require('components/images/common/Bookmark.react'),
-    context = require('context'),
-    Tags = require('components/images/detail/tags/Tags.react'),
-    stores = require('stores'),
-    Showdown = require('showdown'),
-    globals = require('globals'),
-    moment = require('moment'),
-    momentTZ = require('moment-timezone'),
-    Router = require('react-router'),
-    MediaCard = require('components/common/ui/MediaCard.react');
-
-  return React.createClass({
+export default React.createClass({
     displayName: "ImageListCard",
 
-    getInitialState() {
-        return {
-            isExpanded: false
+    propTypes: {
+        image: React.PropTypes.instanceOf(Backbone.Model).isRequired
+    },
+
+    onBookmark: function(image) {
+        let imageBookmark = stores.ImageBookmarkStore.findOne({
+            'image.id': image.id
+        });
+        if (imageBookmark) {
+            actions.ImageBookmarkActions.removeBookmark({image});
+        } else {
+            actions.ImageBookmarkActions.addBookmark({image});
         }
     },
 
-    propTypes: {
-      image: React.PropTypes.instanceOf(Backbone.Model).isRequired
+    onLaunchImage: function(image) {
+        modals.InstanceModals.launch({ image, initialView: "BASIC_VIEW" });
     },
 
-    onExpand() {
-        let isExpanded = this.state.isExpanded ?
-            false : true;
-        this.setState({
-            isExpanded
-        });
-        this.props.onExpand()
-    },
+    render: function() {
+        let image = this.props.image;
+        let type = stores.ProfileStore.get().get('icon_set');
+        let imageTags = stores.TagStore.getImageTags(image);
 
-    onBookmark(image) {
-        console.log("bookmark", image);
-    },
+        let imageCreationDate = moment(image.get('start_date'))
+            .tz(globals.TZ_REGION)
+            .format("MMM Do YYYY hh:mm a z");
 
-    onLaunchImage(image) {
-        console.log("Launch", image);
-    },
+        let converter = new Showdown.Converter();
+        let descriptionFull = image.get('description');
+        let description;
+        let descriptionActive;
 
-    render() {
-      let image = this.props.image;
-      let type = stores.ProfileStore.get().get('icon_set');
-      let imageTags = stores.TagStore.getImageTags(image);
-        
-      let imageCreationDate = moment(image.get('start_date'))
-        .tz(globals.TZ_REGION)
-        .format("MMM Do YYYY hh:mm a z");
-      let converter = new Showdown.Converter();
-      let descriptionFull = image.get('description');
-      let description;
-      let descriptionActive;
-
-      if (descriptionFull) {
+        if (descriptionFull) {
             description = descriptionFull.length < 150 ? 
                 descriptionFull :
                 (descriptionFull.substring(0,150) + " ...");
+
             descriptionActive = description;
-      }
-      if (!descriptionFull) {
+        }
+
+        if (!descriptionFull) {
             descriptionActive = "No Description Provided."
-      }
+        }
 
-      if (this.props.isExpanded) {
-          descriptionActive = descriptionFull;
+        if (this.props.isExpanded) {
+            descriptionActive = descriptionFull;
 
-      }
+        }
 
-      let iconSize = 40;
-
-      // always use the Gravatar icons
-      let icon = (
+        // always use the Gravatar icons
+        let icon = (
             <Router.Link to="image-details" params={{imageId: image.id}}>
-                    <Gravatar hash={image.get('uuid_hash')} size={iconSize} type={type}/>
+                    <Gravatar hash={image.get('uuid_hash')} size={40} type={type}/>
             </Router.Link>
-      );
+        );
 
-      let title = (
+        let title = (
             <Router.Link to="image-details" params={{imageId: image.id}}>
                 {image.get('name')}
             </Router.Link>
-      );
+        );
 
-      let date = (
+        let date = (
             <div className="Media-date">
                 <time>{imageCreationDate}</time> by <strong>{image.get('created_by').username}</strong>
             </div>
-      );
+        );
 
-      let detail = (
+        let detail = (
             <div>
                 <div className="Media-description col-md-6">
                     {descriptionActive}
@@ -102,64 +95,57 @@ define(function (require) {
                     <Tags activeTags={imageTags}/>
                 </div>
             </div>
-      );
-
-      // Hide bookmarking on the public page
-      var bookmarkButton;
-      if (context.profile) {
-        bookmarkButton = (
-          <Bookmark image={image}/>
         );
-      };
 
-      let isFeatured = imageTags.some(
-              (item) => item.get('name') === "Featured"
-      );
-      let isBookmarked= stores.ImageBookmarkStore.findOne({
-          'image.id': image.id
-      });
-      let Featured = isFeatured ? 
-          (
-           <li style={{listStyle: "none", padding: "0 5px 0 0", display: "inline-block" }}>
-            <i className="glyphicon glyphicon-thumbs-up"/>
-           </li>) :
-          null;
-      let Bookmarked = isBookmarked ?
-          (
-           <li style={{listStyle: "none", padding: "0 5px 0 0", display: "inline-block" }}>
-            <i className="glyphicon glyphicon-star"/>
-           </li>) :
-          null;
+        let isFeatured = imageTags.some(
+            (item) => item.get('name') === "Featured"
+        );
+        let isBookmarked= stores.ImageBookmarkStore.findOne({
+            'image.id': image.id
+        });
+        let Featured = isFeatured ?
+            (
+                <li style={{listStyle: "none", padding: "0 5px 0 0", display: "inline-block" }}>
+                    <i className="glyphicon glyphicon-star"/>
+                </li>
+            ) : null;
 
-      let imageIcons = (
-              <ul style={{padding: "0",color: "grey"}}>
+        let Bookmarked = isBookmarked ?
+            (
+                <li style={{listStyle: "none", padding: "0 5px 0 0", display: "inline-block" }}>
+                    <i className="glyphicon glyphicon-bookmark"/>
+                </li>
+            ) : null;
+
+        let imageIcons = (
+            <ul style={{padding: "0",color: "grey"}}>
                 {Featured}
                 {Bookmarked}
-              </ul>
-          );
+            </ul>
+        );
 
-      return (
-            <MediaCard 
+        let  bookmarkName = isBookmarked ? "Remove from Bookmarks" : "Add to Bookmarks";
+        let  contextualMenu = context.profile ?
+            [{
+                name: bookmarkName,
+                action: this.onBookmark.bind(this, image)
+            },
+            {
+                name: "Launch Instance",
+                action: this.onLaunchImage.bind(this, image)
+            }] : null;
+
+        return (
+            <MediaCard
                 isExpanded={this.props.isExpanded}
                 image={icon}
                 title={title}
                 subTitle={date}
                 detail={detail}
-                header={[bookmarkButton]}
                 titleInfo={imageIcons}
-                onExpand={this.onExpand}
-                contextualMenu={[
-                    {
-                        name: "Add to Bookmarks",
-                        action: this.onBookmark.bind(this, image)
-                    },
-                    {
-                        name: "Launch Instance",
-                        action: this.onLaunchImage.bind(this, image)
-                    }
-                ]}
+                onExpand={this.props.onExpand}
+                contextualMenu={contextualMenu}
             />
-      );
+        );
     }
-  });
 });
