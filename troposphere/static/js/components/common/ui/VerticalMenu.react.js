7b3a40410794376956de347a6d9b132567e4831d
Tharon Carlson <design@tharoncarlson.com>
2016-04-03 13:07:38 -0700

    improve vertical Menu layout

--- troposphere/static/js/components/common/ui/VerticalMenu.react.js
+++ troposphere/static/js/components/common/ui/VerticalMenu.react.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 export default React.createClass({
-
     getInitialState() {
         return({
             isOpen: false,
         })
+    },
+
+    componentDidMount: function() {
+        document.addEventListener('click', this.handleDocumentClick, false);
+    },
+
+    componentWillUnmount: function() {
+        document.removeEventListener('click', this.handleDocumentClick, false);
+    },
+
+    handleDocumentClick: function(e) {
+        let menu = this.refs.root.getDOMNode();
+        if (!menu.contains(e.target) && this.state.isOpen) {
+            this.setState({ isOpen: false });
+        }
     },
 
     onTouch(e) {
@@ -57,7 +71,7 @@ export default React.createClass({
                 }}
                 onClick={this.onSelectOption.bind(this, item.action)}
             >
-                {item.name}
+                {item.render}
             </li>
         )
     },
@@ -74,40 +88,40 @@ export default React.createClass({
             { color: "grey" };
 
         return (
-            <div>
-            <div
-                style={{
-                    ...openMenuStyle,
-                    cursor: "pointer",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    fontSize: "17px",
-                    paddingLeft: "2px",
-                    borderRadius: "50%",
-                    border: "solid 1px rgba(0,0,0,.1)",
-                    height: "35px",
-                    width: "35px",
-                }}
-                onClick={this.onTouch}
-            >
-                <i style={{top:"0"}} className="glyphicon glyphicon-option-vertical"/>
-            </div>
-            <ul 
-                style={{
-                    display: showList,
-                    width: "200px",
-                    position: "absolute",
-                    background: "white",
-                    right: "0",
-                    top: "40px",
-                    padding: "6px 0",
-                    zIndex: "200",
-                    boxShadow: "0 -1px 0 #e5e5e5, 0 0 2px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.24)",
-                }}
-            >
-                {menuItemList}
-            </ul>
+            <div ref="root">
+                <div
+                    style={{
+                        ...openMenuStyle,
+                        cursor: "pointer",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        fontSize: "17px",
+                        paddingLeft: "2px",
+                        borderRadius: "50%",
+                        border: "solid 1px rgba(0,0,0,.1)",
+                        height: "35px",
+                        width: "35px",
+                    }}
+                    onClick={this.onTouch}
+                >
+                    <i style={{top:"0"}} className="glyphicon glyphicon-option-vertical"/>
+                </div>
+                <ul 
+                    style={{
+                        display: showList,
+                        width: "200px",
+                        position: "absolute",
+                        background: "white",
+                        right: "0",
+                        top: "40px",
+                        padding: "6px 0",
+                        zIndex: "200",
+                        boxShadow: "0 -1px 0 #e5e5e5, 0 0 2px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.24)",
+                    }}
+                >
+                    {menuItemList}
+                </ul>
             </div>
         )
     },
