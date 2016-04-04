a716f2e1bd7470ef255bad5445003198ef35a477
Joseph Garcia <prosif@gmail.com>
2016-04-04 14:53:36 -0700

    add shared with list, parent machine info

--- troposphere/static/js/components/admin/ImageRequest.react.js
+++ troposphere/static/js/components/admin/ImageRequest.react.js
@@ -13,8 +13,16 @@ define(function (require) {
 
     getInitialState: function(){
       return{
+        request: stores.ImageRequestStore.get(this.getParams().id),
         response: ""
       }
+    },
+
+    componentDidMount: function(){
+        var parentMachineId = this.state.request.get('parent_machine').id;
+        stores.ProviderMachineStore.onFetchModel(parentMachineId, function(){
+            this.setState({parentMachine: stores.ProviderMachineStore.get(parentMachineId)});
+        }.bind(this));
     },
 
     handleResponseChange: function (event) {
@@ -63,7 +71,14 @@ define(function (require) {
 
     render: function () {
       var request = stores.ImageRequestStore.get(this.getParams().id),
-          instance = request.get('instance');
+          instance = request.get('instance'),
+          parentMachine = this.state.parentMachine;
+
+      if(!parentMachine){
+        return <div className="loading" />
+      }
+
+      var basicInfo, detailedInfo, parentInfo;
 
       // ensure boolean values are displayed as strings
       var allowImaging = "false";
@@ -77,33 +92,78 @@ define(function (require) {
         allowImaging = "true";
       }
 
+      // The API returns a string representation of the list. Convert that string back to an array
+      var accessListArray = request.get('access_list').replace(/["' []]*/g, '').split(',');
+
+      var sharedWithText, accessListText;
+      if(request.get('new_application_visibility') == 'private'){
+        accessListText = accessListArray.map(function(name){
+            return(
+                <li key={name}>{name}</li>
+            )
+        });
+
+        sharedWithText = (
+            <span>
+                <label>Shared with:</label>
+                <ul>{accessListText}</ul>
+            </span>
+        );
+      }
+
+      parentInfo = (
+        <span>
+        <label>Parent Machine:</label>
+        <ul>
+            <li>
+                <label>ID:</label> {parentMachine.id}
+            </li>
+            <li>
+                <label>Image:</label> {parentMachine.get('image').name}
+            </li>
+            <li>
+                <label>Version:</label> {parentMachine.get('version').name}
+            </li>
+        </ul>
+        </span>
+      );
+
+      basicInfo = (
+        <div className="basic-info pull-left">
+            <label>Request ID:</label> {request.get('id')}<br />
+            <label>Instance ID:</label> {instance.id}<br />
+            <label>Instance name:</label> {instance.name}<br />
+            <label>New application name:</label> {request.get('new_application_name')}<br />
+            <label>New application description:</label> {request.get('new_application_description')}<br />
+            <label>New machine owner:</label> {request.get('new_machine_owner').username}<br />
+            <label>Request state:</label> {request.get('old_status')}<br />
+            <label>Status:</label> {request.get('status').name}<br />
+            <label>New provider:</label> {request.get('new_machine_provider').name}<br />
+            <label>Allow imaging:</label> {allowImaging}<br />
+            <label>Forked:</label> {forked}<br />
+            <label>New version licenses:</label> {request.get('new_version_licenses')}<br />
+            <label>New version memory min:</label> {request.get('new_version_memory_min')}<br />
+            <label>New version cpu min:</label> {request.get('new_version_cpu_min')}<br />
+            <label>New version name:</label> {request.get('new_version_name')}<br />
+            <label>New version scripts:</label> {request.get('new_version_scripts')}<br />
+            <label>New version tags:</label> {request.get('new_version_tags')}<br />
+        </div>
+      );
+
+      detailedInfo = (
+        <div className="detailed-info pull-right">
+            <label>Installed software:</label> {request.get('installed_software')}<br />
+            <label>iPlant sys files:</label> {request.get('iplant_sys_files')}<br />
+            <label>New application visibility:</label> {request.get('new_application_visibility')}<br />
+            {sharedWithText}
+            {parentInfo}
+        </div>
+      );
+
       return(
         <div className="request-admin pull-right admin-detail">
-          <div>
-            <div>Request ID: {request.get('id')}</div>
-            <div>Installed software: {request.get('installed_software')}</div>
-            <div>Instance ID: {instance.id}</div>
-            <div>Instance name: {instance.name}</div>
-            <div>iPlant sys files: {request.get('iplant_sys_files')}</div>
-            <div>New application description: {request.get('new_application_description')}</div>
-            <div>New application name: {request.get('new_application_name')}</div>
-            <div>New machine owner: {request.get('new_machine_owner').username}</div>
-            <div>New provider: {request.get('new_machine_provider').name}</div>
-            <div>Allow imaging: {allowImaging}</div>
-            <div>Forked: {forked}</div>
-          </div>
-
-          <div>
-            <div>New version licenses: {request.get('new_version_licenses')}</div>
-            <div>New version memory min: {request.get('new_version_memory_min')}</div>
-            <div>New version cpu min: {request.get('new_version_cpu_min')}</div>
-            <div>New version name: {request.get('new_version_name')}</div>
-            <div>New version scripts: {request.get('new_version_scripts')}</div>
-            <div>New version tags: {request.get('new_version_tags')}</div>
-            <div>Request state: {request.get('old_status')}</div>
-            <div>Status: {request.get('status').name}</div>
-          </div>
-
+          {basicInfo}
+          {detailedInfo}
           <div className="request-actions">
             <h4>Response:</h4><br />
             <textarea type="text" form="admin" value={this.state.value}
