8209dd2259b2e54aeb2870e5135dde916114264b
Lucas H. Xu <xuh@bu.edu>
2017-09-08 10:16:26 -0400

    latest version

--- troposphere/static/js/components/marketplace/MarketplacePage.jsx
+++ troposphere/static/js/components/marketplace/MarketplacePage.jsx
@@ -3,14 +3,15 @@ import React from "react";
 import context from "context";
 import modals from "modals";
 import stores from "stores";
-
 import CallToAction from "./CallToAction";
+
 
 // images
 import openstack from "themeImages/openstack.png";
 import dataverse from "themeImages/dataverse.jpg";
 import openshift from "themeImages/openshift.png";
 import comming from "themeImages/comming.jpg";
+
 
 export default React.createClass({
     displayName: "MarketplacePage",
@@ -33,7 +34,6 @@ export default React.createClass({
         stores.ImageStore.addChangeListener(this.updateState);
         stores.InstanceStore.addChangeListener(this.updateState);
         stores.VolumeStore.addChangeListener(this.updateState);
-        stores.SizeStore.addChangeListener(this.updateState);
     },
 
     componentWillUnmount: function() {
@@ -45,7 +45,6 @@ export default React.createClass({
         stores.ImageStore.removeChangeListener(this.updateState);
         stores.InstanceStore.removeChangeListener(this.updateState);
         stores.VolumeStore.removeChangeListener(this.updateState);
-        stores.SizeStore.removeChangeListener(this.updateState);
     },
 
     render: function() {
@@ -57,7 +56,7 @@ export default React.createClass({
             images = stores.ImageStore.getAll(),
             instances = stores.InstanceStore.getAll(),
             volumes = stores.VolumeStore.getAll(),
-            sizes = stores.SizeStore.fetchWhereNoCache({
+            sizes = stores.SizeStore.fetchWhere({
                 "archived": "true",
                 "page_size": 250
             });
@@ -71,7 +70,7 @@ export default React.createClass({
                 , images
                 , instances
                 , volumes
-                , sizes].every(obj => obj)
+                , sizes].every(obj => obj);
 
         if (!resourcesLoaded) {
             return <div className="loading"></div>;
@@ -82,38 +81,38 @@ export default React.createClass({
                 <div style={{ paddingTop: "30px" }} className="container">
                     <h2 className="t-headline">Available MOC Services</h2>
                     <div className="row calls-to-action">
-                        <div className="col-md-4 col-sm-12">
+                        <div className="col-xs-6 col-sm-3">
                             <CallToAction title="MOC OpenStack Service"
                                           image={openstack}
                                           description="Launch an instance, view projects, or view current resources."
                                           linksTo="images" />
                         </div>
-                        <div className="col-md-4 col-sm-12">
+                        <div className="col-xs-6 col-sm-3">
                             <CallToAction title="MOC Cloud Dataverse"
                                           image={dataverse}
-                                          description="Data Processing with Sahara and Cloud Dataverse."
+                                          description="Data Processing with OpenStack Sahara and Cloud Dataverse."
                                           linksTo="dataverse" />
                         </div>
-                        <div className="col-md-4 col-sm-12">
+                        <div className="col-xs-6 col-sm-3">
                             <CallToAction title="MOC RedHat OpenShift"
                                           image={openshift}
                                           description="Quickly develop, host, and scale applications in a cloud environment."
                                           linksTo="openshift" />
                         </div>
-                        <div className="col-md-4 col-sm-12">
+                        <div className="col-xs-6 col-sm-3">
                             <CallToAction title="HIL"
                                           image={comming}
-                                          description="Bare metal isolation service that automates allocation and management of isolated pools of non-virtualized compute resources to users."
+                                          description="Allow for reserving physical machines and connecting them via isolated networks."
                                           linksTo="hil" />
                         </div>
                     </div>
                     <h2 className="t-headline">GIJI Enablements</h2>
                     <div className="row calls-to-action">
                         <div className="col-xs-6 col-sm-3">
-                            <CallToAction title="Mix&Match"
+                            <CallToAction title="Bare Metal Imaging"
                                           image={comming}
-                                          description="Proxy Service that will forward REST API requests to a remote service provider which is federated using Keystone-to-Keystone Federation."
-                                          linksTo="mixmatch" />
+                                          description="Provisions numerous nodes quickly while preserving support for multitenancy using HIL introduces the image management techniques."
+                                          linksTo="bmi" />
                         </div>
                         <div className="col-xs-6 col-sm-3">
                             <CallToAction title="NetEx"
@@ -122,10 +121,10 @@ export default React.createClass({
                                           linksTo="netex" />
                         </div>
                         <div className="col-xs-6 col-sm-3">
-                            <CallToAction title="Bare Metal Imaging"
+                            <CallToAction title="Mix&Match"
                                           image={comming}
-                                          description="Provisions numerous nodes quickly while preserving support for multitenancy using HIL introduces the image management techniques."
-                                          linksTo="bmi" />
+                                          description="Proxy Service that will forward REST API requests to a remote service provider which is federated using Keystone-to-Keystone Federation."
+                                          linksTo="mixmatch" />
                         </div>
                     </div>
                 </div>
