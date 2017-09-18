676fc184056ccfc0b56cb163f170ff62df1d8d38
Lucas H. Xu <xuh@bu.edu>
2017-09-18 09:09:29 -0400

    change links on bmi,hil,mixmatch

--- troposphere/static/js/components/hil/HILPage.jsx
+++ troposphere/static/js/components/hil/HILPage.jsx
@@ -11,7 +11,7 @@ export default React.createClass({
             <h1 className="t-display-1">Hardware Isolation Layer</h1>
             <div>
                 <p>
-                    {"Comming soon!"}
+                    {"Comming soon! For now, please go to"}<a href={"https://massopen.cloud/blog/project-hil"} target="_blank"> {"MOC webside"}</a> {"and"} <a href={"https://github.com/CCI-MOC/hil"} target="_blank"> {"Github"}</a> {"for more details"}
                 </p>
             </div>
         </div>
